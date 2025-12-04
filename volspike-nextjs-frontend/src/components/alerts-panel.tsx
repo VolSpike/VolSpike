1d1dae9142b7fd184e3122121d889499f19ca741
Nik Sitnikov <nsitnikov1@gmail.com>
2025-12-04 13:46:22 -0500

    test: revert to side=top to verify overflow fix

--- volspike-nextjs-frontend/src/components/alerts-panel.tsx
+++ volspike-nextjs-frontend/src/components/alerts-panel.tsx
@@ -231,7 +231,7 @@ export function AlertsPanel({ onNewAlert, guestMode = false, guestVisibleCount =
                       </Button>
                     </TooltipTrigger>
                     <TooltipContent
-                      side="bottom"
+                      side="top"
                       className="oi-teaser-tooltip max-w-[240px] p-0 overflow-hidden"
                       sideOffset={4}
                     >
